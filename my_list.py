54725dc2e03ebb5fda6ab037a35cf10f2cc64db0
loice okello <loicelaura@gmail.com>
2024-03-19 12:05:45 +0300

    python OOP Assignment

---
+++ my_list.py
@@ -0,0 +1,30 @@
+
+my_list = []  # Create an empty list
+
+# Append elements to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)  
+
+# Insert value 15 at the second position
+my_list.insert(1, 15)
+print(my_list) 
+
+# Extend my_list with another list
+my_list.extend([50, 60, 70])
+print(my_list)  
+
+# Remove the last element from my_list
+my_list.pop()
+print(my_list) 
+
+# Sort my_list in ascending order
+my_list.sort()
+print(my_list)
+
+# Find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
